51e3ebb5ffbbaf1d085bb034f19ce704251339d8
Steven Jeanneret <steven.jeanneret@outlook.com>
2018-10-11 16:37:43 +0200

    remove span active

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -33,7 +33,7 @@
     <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
             <li class="nav-item {{{ (Request::is('/') ? 'active' : '') }}}">
-                <a class="nav-link" href="{{route('home')}}">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="{{route('home')}}">Home</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link {{{ (Request::is('folders/*') ? 'active' : '') }}}" href="{{route('folders.index')}}">Folder</a>

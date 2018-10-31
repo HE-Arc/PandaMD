bbc58ac1237f5a6b19bb31ff684f05fbd93363a4
Steven Jeanneret <steven.jeanneret@outlook.com>
2018-10-31 10:40:00 +0100

    change config database for server

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,11 +56,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('POSTGRES_HOST', env('DB_HOST', '127.0.0.1')),
+            'port' => env('POSTGRES_PORT', env('DB_PORT', '5432')),
+            'database' => env('GROUPNAME', env('DB_DATABASE', 'forge')),
+            'username' => env('GROUPNAME', env('DB_USERNAME', 'forge')),
+            'password' => env('PASSWORD', env('DB_PASSWORD', '')),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',

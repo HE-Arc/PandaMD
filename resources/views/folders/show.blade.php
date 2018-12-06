4eec44c633d990c620e970c3c6d9ef94eff7abfa
Steven Jeanneret <steven.jeanneret@outlook.com>
2018-12-06 15:18:20 +0100

    header folder with path and insert button inside

--- resources/views/folders/show.blade.php
+++ resources/views/folders/show.blade.php
@@ -1,27 +1,24 @@
 @extends('layout.app')
 @section('content')
 
-    <div class="row mb-3 ">
-        <div class="col-sm-1 col-3 mr-5">
-            <button id="btnNewFolder" type="button" class="btn btn-dark"><i class="far fa-folder-plus"></i> New Folder
-            </button>
-        </div>
-        <div class="col-1">
-            <button id="btnRenameFolder{{$folder->id}}" name="{{$folder->id}}" value="{{$folder->name}}" type="button"
-                    class="btn btn-secondary"><i
-                        class="fal fa-pen"></i> Rename
-            </button>
-        </div>
-    </div>
     <div class="row">
         <div class="col-12">
             <div class="list-group">
-                <a href="javascript:;" class="list-group-item  active bg-dark">
-                    <h1>{{$folder->name}}</h1>
-                </a>
+                <div class="row mx-0 darkBackground border border-dark rounded text-secondary" style="font-size: 30pt;">
+                    <div class="col">
+                        @foreach($folderPath as $folder)
+                            <a href="{{ route('folders.show', $folder->id) }}" class="text-secondary">{{ $folder->name }}</a>@if(!$loop->last)<span style="margin-right:-10px;">/</span>@endif
+                        @endforeach
+                    </div>
+                    <div id="btnNewFolder" class="col-auto actionToHover text-center">
+                            <i class="far fa-plus fa-fw py-2"></i>
+                    </div>
+                </div>
+
                 @foreach($folders as $childFolder)
                     <a href="{{route('folders.show',$childFolder->id)}}" class=" list-group-item clearfix ">
-                        <h3 style="display: inline;"><i class="fal fa-folder fa-fw"></i> <span class="folder{{$childFolder->id}}">{{$childFolder->name}}</span></h3>
+                        <h3 style="display: inline;"><i class="fal fa-folder fa-fw"></i> <span
+                                    class="folder{{$childFolder->id}}">{{$childFolder->name}}</span></h3>
                         <span class="float-right">
                             @include('folders.partials.selectFolderFolder')
                             <button id="btnRenameFolder{{$childFolder->id}}"
@@ -45,7 +42,7 @@
                         <i class="fal fa-file fa-fw"></i> <span class="file{{$file->id}}">{{$file->title}}</span>
                         <span class="float-right">
                             @include('files.partials.selectFileFolder')
-                            <button id="btnRenameFile{{$file->id}}"  value="{{$file->id}}"
+                            <button id="btnRenameFile{{$file->id}}" value="{{$file->id}}"
                                     class="btn  btn-secondary">
                                 <i class="fal fa-pen fa-fw"></i><span class="d-none d-lg-inline"> Rename</span>
                             </button>
@@ -71,10 +68,10 @@
     <script>
         $(document).ready(function () {
             onReadyNewFolder();
-            onReadyRename('btnRenameFolder',"{{route('folders.update',':id')}}","folder")
-            onReadyRename('btnRenameFile',"{{route('changeFileName',':id')}}","file")
-            onReadyDelete('btnDeleteFolder',"{{route('folders.destroy',':id')}}");
-            onReadyDelete('btnDeleteFile',"{{route('files.destroy',':id')}}");
+            onReadyRename('btnRenameFolder', "{{route('folders.update',':id')}}", "folder")
+            onReadyRename('btnRenameFile', "{{route('changeFileName',':id')}}", "file")
+            onReadyDelete('btnDeleteFolder', "{{route('folders.destroy',':id')}}");
+            onReadyDelete('btnDeleteFile', "{{route('files.destroy',':id')}}");
             OnreadyChangeFileFolder();
             OnreadyChangeFolderFolder();
         })

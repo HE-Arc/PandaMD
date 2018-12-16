7ce79fe1b43ef5502e3607d09aebe25852bbb661
thibault.haldenwa <thibault.haldenwang@hotmail.com>
2018-12-16 21:25:51 +0100

    change card title to card header, dark background card headers

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -8,9 +8,9 @@
         <div class="card-group">
             <div class="col-sm">
                 <div class="card bg-light">
+                    <h5 class="card-header bg-dark text-white">Convert markdown to PDF</h5>
                     <img class="card-img-top" src="{{asset('images/MD_TO_PDF.png')}}">
                     <div class="card-body">
-                        <h5 class="card-title">Convert markdown to PDF</h5>
                         <p class="card-text">
                             Convert your markdown files to PDF without installing anything. Just upload your file,
                             select the options you want and let our server run pandoc for you.
@@ -21,9 +21,9 @@
             </div>
             <div class="col-sm">
                 <div class="card bg-light">
+                    <h5 class="card-header bg-dark text-white">Write, save and edit online</h5>
                     <img class="card-img-top" src="{{asset('images/edit_demo2.png')}}">
                     <div class="card-body">
-                        <h5 class="card-title">Write, save and edit online</h5>
                         <p class="card-text">
                             Our built-in editor allows you to take notes directly on
                             our website, and access them from anywhere.
@@ -33,9 +33,9 @@
             </div>
             <div class="col-sm">
                 <div class="card bg-light">
+                    <h5 class="card-header bg-dark text-white">Share your notes</h5>
                     <img class="card-img-top" src="{{asset('images/share_1.png')}}">
                     <div class="card-body">
-                        <h5 class="card-title">Share your notes</h5>
                         <p class="card-text">
                             Thanks to our file system, you can share your notes exactly
                             the way you like: keep them private, allow read only or full edition.
